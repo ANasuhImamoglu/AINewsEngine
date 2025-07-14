a8466ed26bf898611afbd151a2fe21d72352841f
Taha Yiğit Göksu <tahayg2003@gmail.com>
2025-07-14 11:28:04 +0300

    bug fix

--- AINewsEngine/AINewsEngine/Controllers/YerIsaretleriController.cs
+++ AINewsEngine/AINewsEngine/Controllers/YerIsaretleriController.cs
@@ -32,7 +32,7 @@ namespace AINewsEngine.Controllers
 
             var haberler = await _context.YerIsaretleri
                 .Where(y => y.KullaniciId == userId)
-                .Include(y => y.Haber)
+                .Include(y => y.Haber) //include
                 .Select(y => y.Haber) // Sadece ilişkili Haber nesnelerini seç
                 .ToListAsync();
 
